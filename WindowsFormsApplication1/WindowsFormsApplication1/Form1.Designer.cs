ead1171c07ff82fb90baeea0fd23488fe4d94a66
tfcmad <tfcmad2@hotmail.co.uk>
2014-09-16 02:28:06 +0100

    Simple Game.

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class frmOandX
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGrid0 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnGrid2 = new System.Windows.Forms.Button();
+            this.btnGrid1 = new System.Windows.Forms.Button();
+            this.btnGrid4 = new System.Windows.Forms.Button();
+            this.btnGrid5 = new System.Windows.Forms.Button();
+            this.btnGrid3 = new System.Windows.Forms.Button();
+            this.btnGrid7 = new System.Windows.Forms.Button();
+            this.btnGrid8 = new System.Windows.Forms.Button();
+            this.btnGrid6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGrid0
+            // 
+            this.btnGrid0.FlatAppearance.BorderSize = 0;
+            this.btnGrid0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid0.Location = new System.Drawing.Point(15, 31);
+            this.btnGrid0.Name = "btnGrid0";
+            this.btnGrid0.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid0.TabIndex = 0;
+            this.btnGrid0.Text = "-";
+            this.btnGrid0.UseVisualStyleBackColor = false;
+            this.btnGrid0.Click += new System.EventHandler(this.btnGrid0_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(143, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 298);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(282, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 298);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Location = new System.Drawing.Point(11, 111);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(403, 10);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(11, 229);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(403, 10);
+            this.panel4.TabIndex = 4;
+            // 
+            // btnGrid2
+            // 
+            this.btnGrid2.FlatAppearance.BorderSize = 0;
+            this.btnGrid2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid2.Location = new System.Drawing.Point(293, 32);
+            this.btnGrid2.Name = "btnGrid2";
+            this.btnGrid2.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid2.TabIndex = 5;
+            this.btnGrid2.Text = "-";
+            this.btnGrid2.UseVisualStyleBackColor = false;
+            this.btnGrid2.Click += new System.EventHandler(this.btnGrid2_Click);
+            // 
+            // btnGrid1
+            // 
+            this.btnGrid1.FlatAppearance.BorderSize = 0;
+            this.btnGrid1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid1.Location = new System.Drawing.Point(154, 32);
+            this.btnGrid1.Name = "btnGrid1";
+            this.btnGrid1.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid1.TabIndex = 6;
+            this.btnGrid1.Text = "-";
+            this.btnGrid1.UseVisualStyleBackColor = false;
+            this.btnGrid1.Click += new System.EventHandler(this.btnGrid1_Click);
+            // 
+            // btnGrid4
+            // 
+            this.btnGrid4.FlatAppearance.BorderSize = 0;
+            this.btnGrid4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid4.Location = new System.Drawing.Point(154, 133);
+            this.btnGrid4.Name = "btnGrid4";
+            this.btnGrid4.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid4.TabIndex = 9;
+            this.btnGrid4.Text = "-";
+            this.btnGrid4.UseVisualStyleBackColor = false;
+            this.btnGrid4.Click += new System.EventHandler(this.btnGrid4_Click);
+            // 
+            // btnGrid5
+            // 
+            this.btnGrid5.FlatAppearance.BorderSize = 0;
+            this.btnGrid5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid5.Location = new System.Drawing.Point(293, 133);
+            this.btnGrid5.Name = "btnGrid5";
+            this.btnGrid5.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid5.TabIndex = 8;
+            this.btnGrid5.Text = "-";
+            this.btnGrid5.UseVisualStyleBackColor = false;
+            this.btnGrid5.Click += new System.EventHandler(this.btnGrid5_Click);
+            // 
+            // btnGrid3
+            // 
+            this.btnGrid3.FlatAppearance.BorderSize = 0;
+            this.btnGrid3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid3.Location = new System.Drawing.Point(15, 132);
+            this.btnGrid3.Name = "btnGrid3";
+            this.btnGrid3.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid3.TabIndex = 7;
+            this.btnGrid3.Text = "-";
+            this.btnGrid3.UseVisualStyleBackColor = false;
+            this.btnGrid3.Click += new System.EventHandler(this.btnGrid3_Click);
+            // 
+            // btnGrid7
+            // 
+            this.btnGrid7.FlatAppearance.BorderSize = 0;
+            this.btnGrid7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid7.Location = new System.Drawing.Point(154, 248);
+            this.btnGrid7.Name = "btnGrid7";
+            this.btnGrid7.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid7.TabIndex = 12;
+            this.btnGrid7.Text = "-";
+            this.btnGrid7.UseVisualStyleBackColor = false;
+            this.btnGrid7.Click += new System.EventHandler(this.btnGrid7_Click);
+            // 
+            // btnGrid8
+            // 
+            this.btnGrid8.FlatAppearance.BorderSize = 0;
+            this.btnGrid8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid8.Location = new System.Drawing.Point(293, 248);
+            this.btnGrid8.Name = "btnGrid8";
+            this.btnGrid8.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid8.TabIndex = 11;
+            this.btnGrid8.Text = "-";
+            this.btnGrid8.UseVisualStyleBackColor = false;
+            this.btnGrid8.Click += new System.EventHandler(this.btnGrid8_Click);
+            // 
+            // btnGrid6
+            // 
+            this.btnGrid6.FlatAppearance.BorderSize = 0;
+            this.btnGrid6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGrid6.Location = new System.Drawing.Point(15, 247);
+            this.btnGrid6.Name = "btnGrid6";
+            this.btnGrid6.Size = new System.Drawing.Size(126, 77);
+            this.btnGrid6.TabIndex = 10;
+            this.btnGrid6.Text = "-";
+            this.btnGrid6.UseVisualStyleBackColor = false;
+            this.btnGrid6.Click += new System.EventHandler(this.btnGrid6_Click);
+            // 
+            // frmOandX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 362);
+            this.Controls.Add(this.btnGrid7);
+            this.Controls.Add(this.btnGrid8);
+            this.Controls.Add(this.btnGrid6);
+            this.Controls.Add(this.btnGrid4);
+            this.Controls.Add(this.btnGrid5);
+            this.Controls.Add(this.btnGrid3);
+            this.Controls.Add(this.btnGrid1);
+            this.Controls.Add(this.btnGrid2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGrid0);
+            this.Name = "frmOandX";
+            this.Text = "O and X";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGrid0;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnGrid2;
+        private System.Windows.Forms.Button btnGrid1;
+        private System.Windows.Forms.Button btnGrid4;
+        private System.Windows.Forms.Button btnGrid5;
+        private System.Windows.Forms.Button btnGrid3;
+        private System.Windows.Forms.Button btnGrid7;
+        private System.Windows.Forms.Button btnGrid8;
+        private System.Windows.Forms.Button btnGrid6;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//To Do. 
+//Code Turn System
+//Add Ai random choice, Develope to smarter choices
+//Add Rules.
+//Add Win and Lose.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +16,272 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class frmOandX : Form
     {
-        public Form1()
+        board boardDeafault = new board();
+
+        public frmOandX()
         {
             InitializeComponent();
+
         }
+
+        private void btnGrid0_Click(object sender, EventArgs e)
+        {
+
+            if (validate("grid0"))
+            {
+                btnGrid0.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+        }
+
+        private void btnGrid1_Click(object sender, EventArgs e)
+        {
+
+            if (validate("grid1"))
+            {
+                btnGrid1.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid2_Click(object sender, EventArgs e)
+        {
+            if (validate("grid2"))
+            {
+                btnGrid2.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid3_Click(object sender, EventArgs e)
+        {
+            if (validate("grid3"))
+            {
+                btnGrid3.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid4_Click(object sender, EventArgs e)
+        {
+            if (validate("grid4"))
+            {
+                btnGrid4.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid5_Click(object sender, EventArgs e)
+        {
+            if (validate("grid5"))
+            {
+                btnGrid5.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid6_Click(object sender, EventArgs e)
+        {
+            if (validate("grid6"))
+            {
+                btnGrid6.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid7_Click(object sender, EventArgs e)
+        {
+            if (validate("grid7"))
+            {
+                btnGrid7.Text = "X";
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        private void btnGrid8_Click(object sender, EventArgs e)
+        {
+            if (validate("grid8"))
+            {
+                btnGrid8.Text = "X";;
+            }
+            else
+            {
+                errorMessage(1);
+            }
+
+        }
+
+        public void errorMessage(int code)
+        {
+            switch (code)
+            {
+                case 1:
+                    MessageBox.Show("Square Already Taken.");
+                    break;
+                default:
+                    MessageBox.Show("Unknown Error.");
+                    break;
+
+            }
+
+        }
+
+
+        public bool validate(string grid)
+        {
+            //MessageBox.Show("Validate Enter.");
+
+            switch (grid)
+            {
+                case "grid0":
+                    if (boardDeafault.grid0 == "")
+                    {
+                        //MessageBox.Show("True.");
+                        boardDeafault.grid0 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid1":
+                    if (boardDeafault.grid1 == "")
+                    {
+                        boardDeafault.grid1 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid2":
+                    if (boardDeafault.grid2 == "")
+                    {
+                        boardDeafault.grid2 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid3":
+                    if (boardDeafault.grid3 == "")
+                    {
+                        boardDeafault.grid3 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid4":
+                    if (boardDeafault.grid4 == "")
+                    {
+                        boardDeafault.grid4 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid5":
+                    if (boardDeafault.grid5 == "")
+                    {
+                        boardDeafault.grid5 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid6":
+                    if (boardDeafault.grid6 == "")
+                    {
+                        boardDeafault.grid6 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid7":
+                    if (boardDeafault.grid7 == "")
+                    {
+                        boardDeafault.grid7 = "X";
+                        return true;
+                    }
+                    break;
+
+                case "grid8":
+                    if (boardDeafault.grid8 == "")
+                    {
+                        boardDeafault.grid8 = "X";
+                        return true;
+                    }
+                    break;
+
+                default:
+                    return false;
+                    break;
+            }
+
+            return false;
+        }
+
+
     }
+
+
+    public class board
+    {
+        //Below varibles should be changed to be protected via accessor methods.
+        public string grid0 { get; set; }
+        public string grid1 { get; set; }
+        public string grid2 { get; set; }
+        public string grid3 { get; set; }
+        public string grid4 { get; set; }
+        public string grid5 { get; set; }
+        public string grid6 { get; set; }
+        public string grid7 { get; set; }
+        public string grid8 { get; set; }
+
+        public board()
+        {
+            grid0 = "";
+            grid1 = "";
+            grid2 = "";
+            grid3 = "";
+            grid4 = "";
+            grid5 = "";
+            grid6 = "";
+            grid7 = "";
+            grid8 = "";
+
+        }
+
+
+
+    }
+
+
+
 }
